4153176590e7bb98058bb48a7a5f5fe95b11afbd
nazerkekalibek <nazerke.kalibek@narxoz.kz>
2026-02-28 04:35:37 +0500

    Added Mage Hero

---
+++ src/com/narxoz/rpg/hero/Mage.java
@@ -0,0 +1,40 @@
+package com.narxoz.rpg.hero;
+
+public class Mage implements Hero {
+    private final String name;
+    private final int power;
+    private int health;
+
+    public Mage(String name) {
+        this.name = name;
+        this.power = 25;
+        this.health = 80;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getPower() {
+        return power;
+    }
+
+    @Override
+    public void receiveDamage(int amount) {
+        health -= amount;
+        if (health < 0) {
+            health = 0;
+        }
+    }
+
+    @Override
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+}
